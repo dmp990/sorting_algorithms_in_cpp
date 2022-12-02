b77a487b40192832a213595546ec69ca92f7a5a7
dmp990 <asadali.asif.1@gmail.com>
2022-12-02 17:48:51 +0000

    implement helper fn get_max() for count sort

---
+++ utils/get_max.h
@@ -0,0 +1,13 @@
+
+int get_max(int arr[], int size)
+{
+    int max = -1;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
